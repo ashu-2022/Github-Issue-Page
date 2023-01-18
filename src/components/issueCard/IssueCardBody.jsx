66f27188fe8a6b140f547b88c9ff1ffc09904133
ashutosh-pandit <ashutosh@dev.causalfunnel.com>
2023-01-18 13:38:35 +0530

    Added Loading Spinner while fetching data from API

--- src/components/issueCard/IssueCardBody.jsx
+++ src/components/issueCard/IssueCardBody.jsx
@@ -1,37 +1,59 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { issues, comment, pullRequests } from "../../assets";
+import Spinner from 'react-bootstrap/Spinner';
 const URL = "https://api.github.com/repos/facebook/react/issues";
+
 
 const IssueCardBody = () => {
   const [issues, setIssues] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     getIsuues();
-  }, []);
+  }, [page]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollHandler)
+    return () => window.removeEventListener('scroll', scrollHandler)
+  }, [] )
+
+  const scrollHandler = () => {
+    // console.log("scrollHeight", document.documentElement.scrollHeight);
+    // console.log("scrollTop", document.documentElement.scrollTop);
+    // console.log("window.innerHeight", window.innerHeight);
+    if (document.documentElement.scrollTop + window.innerHeight + 50 >= document.documentElement.scrollHeight) {
+      setPage(prev => prev + 1);
+    }
+  }
 
   const getIsuues = async () => {
+    let API_URL = `${URL}?page=${page}`;
+    console.log("API_URL", API_URL);
     axios
-      .get(URL)
+      .get(API_URL)
       .then((result) => {
         console.log("Github Issues Data", result.data);
         setIssues((prev) => [...prev, ...result.data]);
       })
       .catch((error) => {
         console.log("Error occur during fetching Github Issues", error);
-      });
+      })
+      .finally(() => setLoading(false));
   };
+
 
   return (
     <div className="issue-card-body">
       {issues &&
-        issues.map((item) => (
+        issues.map((item, index) => (
           <IssueItem
-            key={item.id}
+            key={index}
             title={item.title}
-            url={item.url}
+            url={item.html_url}
             number={item.number}
-            state={item.state}
             updatedAt={item.created_at}
             user={item.user.login}
             comments={item.comments}
@@ -40,6 +62,12 @@ const IssueCardBody = () => {
             pullRequest={item.pull_request}
           />
         ))}
+      
+      {loading &&
+        <div className="text-center my-2">
+          <Spinner animation="border"  />
+        </div>
+      }
     </div>
   );
 };
@@ -50,7 +78,6 @@ const IssueItem = ({
   title,
   url,
   number,
-  state,
   updatedAt,
   user,
   comments,
@@ -69,8 +96,8 @@ const IssueItem = ({
             {title}
           </a>
           {labels &&
-            labels.map((item) => (
-              <a href={item.url} key={item.id}>
+            labels.map((item, index) => (
+              <a href={item.url} key={index}>
                 <p
                   className="issue-labels"
                   style={{
@@ -103,8 +130,6 @@ const IssueItem = ({
             <img src={pullRequests} alt="pull request" /> 1
           </a>
           }
-
-
         </div>
         <div className="issue-comment" style={{ visibility: !comments ? 'hidden' : ''}}>
           <a href={commentsURL}>
